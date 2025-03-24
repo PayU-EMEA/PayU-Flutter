31a5b8f08b21790ea71754d6ac9d4156aa8dbefb
arkadiusz.wawrzyniak <arkadiusz.wawrzyniak@payu.com>
2025-03-24 07:03:21 +0100

    removed mastercard installments

--- example/lib/features/example/backend/api/api.g.dart
+++ example/lib/features/example/backend/api/api.g.dart
@@ -114,62 +114,6 @@ class _Api implements Api {
   }
 
   @override
-  Future<InstallmentProposal> getCardInstallmentProposals(
-      String proposalId) async {
-    final _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    const Map<String, dynamic>? _data = null;
-    final _result = await _dio.fetch<Map<String, dynamic>>(
-        _setStreamType<InstallmentProposal>(Options(
-      method: 'GET',
-      headers: _headers,
-      extra: _extra,
-    )
-            .compose(
-              _dio.options,
-              'api/v2_1/card-installment-proposals/${proposalId}',
-              queryParameters: queryParameters,
-              data: _data,
-            )
-            .copyWith(
-                baseUrl: _combineBaseUrls(
-              _dio.options.baseUrl,
-              baseUrl,
-            ))));
-    final value = InstallmentProposal.fromJson(_result.data!);
-    return value;
-  }
-
-  @override
-  Future<void> postCardInstallmentProposals(
-    String proposalId,
-    InstallmentResult decision,
-  ) async {
-    final _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    _data.addAll(decision.toJson());
-    await _dio.fetch<void>(_setStreamType<void>(Options(
-      method: 'POST',
-      headers: _headers,
-      extra: _extra,
-    )
-        .compose(
-          _dio.options,
-          'api/v2_1/card-installment-proposals/${proposalId}/decisions',
-          queryParameters: queryParameters,
-          data: _data,
-        )
-        .copyWith(
-            baseUrl: _combineBaseUrls(
-          _dio.options.baseUrl,
-          baseUrl,
-        ))));
-  }
-
-  @override
   Future<TransactionsResponse> getTransactions(String orderId) async {
     final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
